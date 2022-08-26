f3afdc4d8eb677945ab9a87d3c0ede5878bcadd1
Marcin T <yahoospider@gmail.com>
2022-08-26 12:06:04 +0200

    PEP8 cleaning

--- wagi.py
+++ wagi.py
@@ -3,7 +3,7 @@ enter_error = "The item was considered to be wrong. Min 1 kg, max 10 kg. END OF 
 
 print(enter)
 
-waga_paczka= 0              # waga paczki
+waga_paczka = 0              # waga paczki
 nr_paczka = 1               # nr paczki wyslane
 puste_kg_paczka = 0         # ile pustych kg w danej paczce
 
@@ -34,29 +34,20 @@ while True:
         error = True
         print(enter_error)
         break
-
-    suma_waga_element = round(suma_waga_element + waga_element,2)  # sumujemy wyslane kilogramy
+    suma_waga_element = round(suma_waga_element + waga_element, 2)  # sumujemy wyslane kilogramy
     waga_paczka = waga_paczka + waga_element    # dodajemy wagi elementow do paczki
-
-
     if waga_paczka > 20:
         print('---------------------------------------------------------------')
-        print ('Przekroczono 20kg/paczka! Jest w sumie:', waga_paczka, "do wyslania")
-        print ('Aktualny nr paczki to:',  nr_paczka)
-
-        waga_paczka = round (waga_paczka - waga_element,2)
-
+        print('Przekroczono 20kg/paczka! Jest w sumie:', waga_paczka, "do wyslania")
+        print('Aktualny nr paczki to:',  nr_paczka)
+        waga_paczka = round(waga_paczka - waga_element, 2)
         print('Zamykam paczke:', nr_paczka, 'zostaje w niej elementow:', waga_paczka)
-
-        puste_kg_paczka = round( 20 - (waga_paczka),2)  # puste w danej paczce
-        suma_puste_kg_paczka = round(suma_puste_kg_paczka + puste_kg_paczka,2)
-
+        puste_kg_paczka = round(20 - waga_paczka, 2)  # puste w danej paczce
+        suma_puste_kg_paczka = round(suma_puste_kg_paczka + puste_kg_paczka, 2)
         print('Puste kg w tej paczce: ', puste_kg_paczka)
-
         if puste_kg_paczka > max_puste_kg_paczka:
             max_puste_kg_paczka = puste_kg_paczka
             nr_max_puste_kg_paczka = nr_paczka
-
         nr_paczka += 1
         print('Rozpoczynam paczke:', nr_paczka)
         print('---------------------------------------------------------------')
@@ -74,14 +65,12 @@ while True:
         waga_paczka = 0
         puste_kg_paczka = 0
         continue
-
     continue
-
 if not error:
-    nr_paczka=nr_paczka-1
+    nr_paczka = nr_paczka-1
     print("*** Podsumowanie ***")
     print("Liczba paczek wysłanych:", nr_paczka)
     print("Liczba kilogramów wysłanych:", suma_waga_element)
     # suma_pustych = liczba paczek * 20 - liczba kilogramów wysłanych
     print("Suma pustych kilogramów: ", suma_puste_kg_paczka)
-    print("Najwięcej pustych kg miała paczka nr:", nr_max_puste_kg_paczka , "a było to:", max_puste_kg_paczka)
+    print("Najwięcej pustych kg miała paczka nr:", nr_max_puste_kg_paczka, "a było to:", max_puste_kg_paczka)
