d879a4b80281e18cbfa1cb00045085adfb480fec
Marcin T <yahoospider@gmail.com>
2022-09-05 13:45:24 +0200

    a bit of translation

--- wagi.py
+++ wagi.py
@@ -7,10 +7,9 @@ parcel_weight = 0           # total weight of the package
 parcel_number = 1           # the number of the parcel sent
 parcel_empty_kilograms = 0  # how many empty kilos are left in the parcel
 
-suma_puste_kg_paczka = 0    # suma pustych we wszystkich paczkach
-
-max_puste_kg_paczka = 0     # najwiecej pustych kg w paczce
-nr_max_puste_kg_paczka = 1  # numer paczki z najwieksza ilosc pustych kg
+parcel_amount_empty_kilograms = 0       # how many empty kilos are in all parcels
+max_parcel_empty_kilograms = 0          # most empty kilograms in the parcel
+nr_max_parcel_empty_kilograms = 1       # parcel number with the most empty kilograms
 
 waga_element = 0            # waga elementu pobrana z inta
 suma_waga_element = 0       # suma kg pobranych z inta
@@ -43,24 +42,24 @@ while True:
         parcel_weight = round(parcel_weight - waga_element, 2)
         print('Zamykam paczke:', parcel_number, 'zostaje w niej elementow:', parcel_weight)
         parcel_empty_kilograms = round(20 - parcel_weight, 2)  # puste w danej paczce
-        suma_puste_kg_paczka = round(suma_puste_kg_paczka + parcel_empty_kilograms, 2)
+        parcel_amount_empty_kilograms = round(parcel_amount_empty_kilograms + parcel_empty_kilograms, 2)
         print('Puste kg w tej paczce: ', parcel_empty_kilograms)
-        if parcel_empty_kilograms > max_puste_kg_paczka:
-            max_puste_kg_paczka = parcel_empty_kilograms
-            nr_max_puste_kg_paczka = parcel_number
+        if parcel_empty_kilograms > max_parcel_empty_kilograms:
+            max_parcel_empty_kilograms = parcel_empty_kilograms
+            nr_max_parcel_empty_kilograms = parcel_number
         parcel_number += 1
-        print('Rozpoczynam paczke:', parcel_number)
+        print('Starting parcel no:', parcel_number)
         print('---------------------------------------------------------------')
         parcel_weight = waga_element    # te kg zostaja do next parcel
         parcel_empty_kilograms = 0
 #   break
     if parcel_weight < 20:
-        print('Aktualna waga paczki to', parcel_weight)
-        print('Aktualny nr paczki to:', parcel_number)
+        print('Current parcel weight is:', parcel_weight)
+        print('Current parcel no is:', parcel_number)
         parcel_empty_kilograms = 0
     if parcel_weight == 20:
-        print('Aktualna waga paczki to', parcel_weight, "rozpoczynam next paczke")
-        print('Zamykam paczke nr:', parcel_number)
+        print('Current parcel weight is:', parcel_weight, "Starting next parcel")
+        print('I am closing the parcel no', parcel_number)
         parcel_number += 1
         parcel_weight = 0
         parcel_empty_kilograms = 0
@@ -72,5 +71,5 @@ if not error:
     print("Number of packages shipped:", parcel_number)
     print("Number of kilos shipped:", suma_waga_element)
     # suma_pustych = liczba paczek * 20 - liczba kilogramów wysłanych
-    print("Total empty kilos: ", suma_puste_kg_paczka)
-    print("Most empty kilos was in the package number:", nr_max_puste_kg_paczka, "this was:", max_puste_kg_paczka)
+    print("Total empty kilos: ", parcel_amount_empty_kilograms)
+    print("Most empty kilos was in the package number:", nr_max_parcel_empty_kilograms, "this was:", max_parcel_empty_kilograms)
