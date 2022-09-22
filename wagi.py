300ab4af9a7d105eef1d78150712d9425052a86a
Marcin T <yahoospider@gmail.com>
2022-09-22 11:28:32 +0200

    more translation

--- wagi.py
+++ wagi.py
@@ -11,8 +11,8 @@ parcel_amount_empty_kilograms = 0       # how many empty kilos are in all parcel
 max_parcel_empty_kilograms = 0          # most empty kilograms in the parcel
 nr_max_parcel_empty_kilograms = 1       # parcel number with the most empty kilograms
 
-waga_element = 0            # waga elementu pobrana z inta
-suma_waga_element = 0       # suma kg pobranych z inta
+element_weight = 0            # element weight taken from the int
+sum_element_weight = 0       # sum of kilos taken from the int
 nr_element = 0              # nr kolejnego elementu
 
 step = 0
@@ -20,26 +20,26 @@ error = False
 while True:
     step += 1
     print("Element nr ", step, "waga:")
-    waga_element = float(input())
-    if waga_element == 0:
+    element_weight = float(input())
+    if element_weight == 0:
         error = False
 # print(enter_error)
         break
-    if waga_element > 10:
+    if element_weight > 10:
         error = True
         print(enter_error)
         break
-    if waga_element < 1:
+    if element_weight < 1:
         error = True
         print(enter_error)
         break
-    suma_waga_element = round(suma_waga_element + waga_element, 2)  # sum od the sent items
-    parcel_weight = parcel_weight + waga_element    # added weight of items to the parcel
+    suma_element_weight = round(sum_element_weight + element_weight, 2)  # sum od the sent items
+    parcel_weight = parcel_weight + element_weight    # added weight of items to the parcel
     if parcel_weight > 20:
         print('---------------------------------------------------------------')
         print('Exceeded 20 kg in parcels! It is in total:', parcel_weight, "to be shipped")
         print('The current parcel number is:',  parcel_number)
-        parcel_weight = round(parcel_weight - waga_element, 2)
+        parcel_weight = round(parcel_weight - element_weight, 2)
         print('I close the parcel number:', parcel_number, 'there are elements in it:', parcel_weight)
         parcel_empty_kilograms = round(20 - parcel_weight, 2)  # empty kilograms in parcel
         parcel_amount_empty_kilograms = round(parcel_amount_empty_kilograms + parcel_empty_kilograms, 2)
@@ -50,7 +50,7 @@ while True:
         parcel_number += 1
         print('Starting parcel no:', parcel_number)
         print('---------------------------------------------------------------')
-        parcel_weight = waga_element    # kilograms to the next parcel
+        parcel_weight = element_weight    # kilograms to the next parcel
         parcel_empty_kilograms = 0
 #   break
     if parcel_weight < 20:
@@ -69,8 +69,8 @@ if not error:
     parcel_number = parcel_number-1
     print("*** Summary ***")
     print("Number of packages shipped:", parcel_number)
-    print("Number of kilos shipped:", suma_waga_element)
-    # suma_pustych = liczba paczek * 20 - liczba kilogramów wysłanych
+    print("Number of kilos shipped:", suma_element_weight)
+    # sum of empty kilos = parcel number * 20 - sent kilos
     print("Total empty kilos: ", parcel_amount_empty_kilograms)
     print("Most empty kilos was in the package number:", nr_max_parcel_empty_kilograms,
           "this was:", max_parcel_empty_kilograms)
